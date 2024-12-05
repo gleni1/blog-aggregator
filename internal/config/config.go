7b512b9f1fb32ff5c571fad96817585ea5106d12
gleni1 <glenipoleshi@gmail.com>
2024-12-05 18:21:48 -0500

    Finished writing the Read function and the SetUser method that changes the userName

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,13 +2,72 @@ package config
 
 import (
   "fmt"
+  "encoding/json"
+  "path/filepath"
+  "log"
+  "os"
 )
+
+const configFileName = ".gatorconfig.json"
 
 type Config struct {
   DbURL           string `json:"db_url"`
   CurrentUserName string `json:"current_user_name"` 
 }
 
-func DoSth () {
-  fmt.Println("Hello from the inner package")
+func Read() (Config, error) {
+  homeDir, err := os.UserHomeDir()
+  if err != nil {
+    return Config{}, fmt.Errorf("error getting home directory")
+  }
+
+  pathName := fmt.Sprintf("%s/%s", homeDir, configFileName)
+
+  file, err := os.Open(pathName)
+  if err != nil {
+    log.Fatalf("Error opening file: %v", err)
+  }
+  defer file.Close()
+
+  var config Config 
+  decoder := json.NewDecoder(file)
+  err = decoder.Decode(&config)
+  if err != nil {
+    return Config{}, fmt.Errorf("error decoding JSON: %v", err)
+  }
+
+  return config, nil 
 }
+
+func (config *Config) SetUser(userName string) error {
+  config.CurrentUserName = userName
+  jsonData, err := json.Marshal(config)
+  if err != nil {
+    return fmt.Errorf("error marshaling config to JSON: %w", err)
+  }
+
+  pathName, err := getConfigFilePath(configFileName)
+  if err != nil {
+    return fmt.Errorf("error getting config filepath: %w", err)
+  }
+
+  err = os.WriteFile(pathName, jsonData, 0644)
+  if err != nil {
+    return fmt.Errorf("error writing to the config file: %w", err)
+  }
+  return nil
+}
+
+func getConfigFilePath(configFileName string) (string ,error) {
+  homeDir, err := os.UserHomeDir()
+  if err != nil {
+    return "", fmt.Errorf("error getting home directory: %w", err)
+  }
+  pathName := filepath.Join(homeDir, configFileName)
+  return pathName, nil
+}
+
+func write(cfg Config) error {
+  return nil  
+}
+
--- main.go
+++ main.go
@@ -3,9 +3,28 @@ package main
 import (
   "fmt"
   "blog/internal/config"
+  "log"
 )
 
 func main() {
-  fmt.Println("Hello World")
-  config.DoSth()
+  if err := run(); err != nil {
+    log.Fatalf("Application error: %w", err)
+  }
+}
+
+func run () error {
+  c, err := config.Read()
+  if err != nil {
+    log.Fatalf("Error reading config: %v", err)
+  }
+  fmt.Printf("Config: %+v\n",c)
+
+  newUserName := "Glen"
+  err = c.SetUser(newUserName)
+  if err != nil {
+    log.Fatalf("Error setting new username: %v", err)
+  }
+
+  fmt.Printf("Config: %+v\n", c)
+  return nil
 }
